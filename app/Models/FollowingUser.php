d0be39fb21152e1b4a00c6eadbf28c3fa480650d
kinoshita0623 <kuutarousii@gmail.com>
2020-10-21 01:10:22 +0900

    フォロー関連の情報を取得しやすくした

--- app/Models/FollowingUser.php
+++ app/Models/FollowingUser.php
@@ -5,9 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Pivot;
+use App\Models\User;
+
 
 class FollowingUser extends Pivot
 {
 
     protected $table = 'following_users';
+
+    /**
+     * フォローしている相手
+     */
+    function followingUser(){
+        return $this->belongsTo(User::class, 'following_user_id');
+    }
+
+    /**
+     * 自分
+     */
+
+     function user(){
+         return $this->belongsTo(User::class, 'user_id');
+     }
 }
