02ac8c3a08872a49acde17552d0c7acc31c74115
vagrant <vagrant@linksharing>
2020-10-22 01:43:02 +0000

    Userのテストを作成した

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,9 +6,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
-
+use PHPUnit\Framework\Assert;
 class UserTest extends TestCase
 {
+
+    use RefreshDatabase;
     
 
     function testCreateUser(){
@@ -16,9 +18,40 @@ class UserTest extends TestCase
         $users = User::factory()->count(10)->create();
 
 
-        assertTrue(count($users) == 10);
+        Assert::assertTrue(count($users) == 10);
         
 
 
+    }
+
+    function testFindById(){
+        $users = User::factory()->count(10)->create();
+        print(gettype($users));
+
+        $user = $users->get(3);
+
+        Assert::assertEquals(10, count($users));
+        Assert::assertNotNull($user);
+
+        var_dump(gettype($user));
+
+        $findById = User::where('id', '=', $user->id)->first();
+
+        //var_dump($user);
+        Assert::assertNotNull($findById);
+        Assert::assertEquals($user->id, $findById->id);
+        
+    }
+
+    function testFindByEmail(){
+        $users = User::factory()->count(10)->create();
+        print(gettype($users));
+
+        $user = $users->get(3);
+
+        $found = User::where('email', '=', $user->email)->first();
+
+        Assert::assertNotNull($found);
+        Assert::assertEquals($user->id, $found->id);
     }
 }
