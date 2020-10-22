cb4ee896c9eedb9f4e23bfd0d2e4c04f98a61397
vagrant <vagrant@linksharing>
2020-10-22 03:26:52 +0000

    テストした

--- tests/Feature/TimelineTest.php
+++ tests/Feature/TimelineTest.php
@@ -14,8 +14,9 @@ class TimelineTest extends TestCase
     
     use RefreshDatabase;
 
-    /*private $targetUser = null;
+    private $targetUser = null;
     private $followings = null;
+    private $followers = null;
 
     function setUp(): void{
         parent::setUp();
@@ -40,34 +41,51 @@ class TimelineTest extends TestCase
             $follow->notes()->saveMany(Note::factory()->count(10)->make());
         }
 
+        // 非相互のフォロワー
+        $followers = User::factory()->count(10)->create();
+        $this->followers = $followers;
+        foreach($followers as $follower){
+            $follower->followings()->attach($this->targetUser);
+        }
+
+
     }
-*/
+
     function testTimeline(){
 
-        $targetUser = User::factory()->create();
+        
+        Assert::assertEquals(100, count($this->targetUser->followings()->get()));
 
-        // フォローしているユーザーを作成する
-        $followingUsers = User::factory()->count(100)->create();
-
-        foreach($followingUsers as $follow){
-            $targetUser->followings()->attach($follow);
-
-        }
-
-
-        $followings = $followingUsers;
-
-        // フォローしているユーザーにNoteを１０個ずつ持たせる
-        foreach($followingUsers as $follow){
-            $follow->notes()->saveMany(Note::factory()->count(10)->make());
-        }
-
-        Assert::assertEquals(100, count($targetUser->followings()->get()));
-
-        foreach($targetUser->followings()->get() as $follow){
+        foreach($this->targetUser->followings()->get() as $follow){
             Assert::assertEquals(10, count($follow->notes()->get()));
         }
-        $count = count($targetUser->timeline()->get());
+        $count = count($this->targetUser->timeline()->get());
+        Assert::assertEquals(1000, $count);
+    }
+
+    /**
+     * 他ユーザーのタイムラインが混信しないことを確認する
+     */
+    function testMixedOtherUsersTimeline(){
+
+        $otherUsers = User::factory()->count(100)->create();
+        foreach($otherUsers as $other){
+            $other->notes()->saveMany(Note::factory()->count(4)->make());
+        }
+
+        $count = count($this->targetUser->timeline()->get());
+        Assert::assertEquals(1000, $count);
+
+        
+    }
+
+    function testNotMixedOneSidedNotFollowersTimeline(){
+        $otherUsers = $this->followers;
+        foreach($otherUsers as $other){
+            $other->notes()->saveMany(Note::factory()->count(4)->make());
+        }
+
+        $count = count($this->targetUser->timeline()->get());
         Assert::assertEquals(1000, $count);
     }
 }
