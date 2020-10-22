213d8a35d6830b03998bf32f854b1169df46ab47
vagrant <vagrant@linksharing>
2020-10-22 01:19:31 +0000

    pgsqlならmigrationに成功したmysqlゴミ2度と使わない

--- database/migrations/2020_10_20_120745_create_following_users_table.php
+++ database/migrations/2020_10_20_120745_create_following_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFollowingUserTable extends Migration
+class CreateFollowingUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -33,6 +33,6 @@ class CreateFollowingUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('following_user');
+        Schema::dropIfExists('following_users');
     }
 }
