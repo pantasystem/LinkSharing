758d0cd7a0f031c4c8265c5a0356729659bf6deb
kinoshita0623 <kuutarousii@gmail.com>
2020-10-27 20:00:48 +0900

    NotificationService

---
+++ app/Services/NotificationService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Notification;
+use App\Models\Comment;
+use App\Models\Favorite;
+use App\Models\FollowingUser;
+
+
+class NotificationService
+{
+
+    public function create(User $publisher, Model $model): Notification
+    {
+        $notification = new Notification;
+
+        $notification->publish($publisher);
+
+        if($model instanceof Comment){
+            $notification->publisher()->associate($model->user);
+
+            $to = $model->replyToComment ?? $model->replyToNote;
+            $notification->subscribe($to);
+            $notification->comment()->associate($model);
+
+        }else if($model instanceof Favorite){
+            $notification->publish($model->user()->get());
+            $notification->subscribe($model->note()->author()->get());
+            $notification->favorite()->associate($model);
+        }else if($model instanceof FollowingUser){
+            $notification->publish($model->user);
+            $notification->subscribe($model->followingUser);
+            $notification->follow($model);
+        }else{
+            return null;
+        }
+
+        $notification = $notification->saveOrFail();
+
+        // 将来的にはPush通知関連のQueueへのPush操作も実装する
+
+        return $notification;
+
+
+    }
+
+}
