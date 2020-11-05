023bfe5925cde10173378cb41a8a9237d63b6ccc
kinoshita0623 <kuutarousii@gmail.com>
2020-11-05 17:05:40 +0900

    とりあえずTextFieldを作った

--- resources/js/components/TextFieldComponent.vue
+++ resources/js/components/TextFieldComponent.vue
@@ -1,56 +1,109 @@
 <template>
+<div class="form-group row">
     <div class="col-md-6">
-        <label for="input-text-field" class="text-md-left">{{ hint }}}</label>
+        <label v-if="hint" for="input-text-field" class="text-md-left">{{ hint }}</label>
+
         <input 
             id="input-text-field" 
             :type="type"
             class="form-control" 
             :class="{ 'is-invalid': error }" 
-            :value="props.error" 
-            :autocomplete="autocomplete"
+            :value="error" 
             :placeholder="hint"
-            :autofocus="autofocus"
+            
             @input="onChanged"
             :required="required"
+            :autofocus="autofocus"
+
+            :autocomplete="autocomplete"
             
         >
+        <!--
+            
+            -->
 
         <span class="invalid-feedback" role="alert" v-if="error">
-            <strong>{{ props.error }}</strong>
+            <strong>{{ error }}</strong>
         </span>
     </div>
+</div>
 </template>
 <script>
 export default {
-    props: {
-        error: {
-            type: 'String'
-        },
-        required: {
+    //props: {
+        /*error: {
+            type: 'String',
+            default: '',
+            required: false
+        },*/
+        /*required: {
             type: 'Boolean',
-            default: false
+            default: false,
+            required: false
+
         },
         autofocus: {
             type: 'Boolean',
             default: false,
+            required: false
         },
         type: {
             type: 'String',
+            default: 'text',
+            required: false
+        },*/
+        /*hint: {
+            
+            type: 'String',
+            default: '',
+            required: false
+        },*/
+        /*autocomplete: {
+            type: 'String',
+            default: 'on',
+            required: false
+        }*/
+
+    //},
+    //props: ['hint', 'error', 'type'],
+    props: {
+        hint: {
+            type: String,
+            required: true
+        },
+        error: {
+            type: String,
+            required: false,
+            default: ''
+        },
+        type: {
+            type: String,
+            required: false,
             default: 'text'
         },
-        hint: {
-            type: ['String', 'Number']
+        required: {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+        autofocus: {
+            type: Boolean,
+            default: false,
+            required: false
         },
         autocomplete: {
-            type: 'String',
-            default: 'on'
+            type: String,
+            default: 'on',
+            required: false
         }
-
     },
     methods: {
         onChanged(event){
             this.$emit('change', event.target.value);
         }
+    },
+    mounted(){
+        console.log(this);
     }
 }
 </script>
