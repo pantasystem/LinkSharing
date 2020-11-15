1596480ee2c822d31a442411a729dd2c82da2fd9
kinoshita0623 <kuutarousii@gmail.com>
2020-11-16 07:52:02 +0900

    Notificationを追加

---
+++ resources/js/components/NotificationComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="contaienr mb-2">
+        
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        notification: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
