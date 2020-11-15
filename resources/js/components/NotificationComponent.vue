2a76aac88e060e4c35dba9441de9a0d6878111c9
kinoshita0623 <kuutarousii@gmail.com>
2020-11-16 08:02:27 +0900

    通知のpublisher部分を実装

--- resources/js/components/NotificationComponent.vue
+++ resources/js/components/NotificationComponent.vue
@@ -1,15 +1,28 @@
 <template>
     <div class="contaienr mb-2">
-        
+        <div class="row">
+            <router-link class="col-1" :to="{ name: 'user_notes', params: {userId: notification.publisher.id}}">
+                <avatar-icon :avatar_icon="notification.publisher.avatar_icon"/>
+            </router-link>
+            <router-link class="col-11" :to="{ name: 'user_notes', params: {userId: notification.publisher.id}}"> 
+                {{ notiifcation.publisher.user_name }}
+            </router-link>
+        </div>
+
     </div>
 </template>
 <script>
+import AvatarIcon from './AvatarIconComponent';
+
 export default {
     props: {
         notification: {
             type: Object,
             required: true
         }
+    },
+    components: {
+        'avatar-icon': AvatarIcon
     }
 }
 </script>
