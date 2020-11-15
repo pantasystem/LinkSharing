1fd1bb1de8d6f307d99c97a98bc2321b144d6550
kinoshita0623 <kuutarousii@gmail.com>
2020-11-15 17:47:13 +0900

    画面遷移時に画面が更新されていなかったので修正した

--- resources/js/components/UserListComponent.vue
+++ resources/js/components/UserListComponent.vue
@@ -6,14 +6,17 @@
         <div class="card-body">
             <div v-for="user in users" :key="user.id">
                 <slot>
-                    <div class="row">
+                    <div class="row mt-2 mb-2">
                         <div class="col-2 wrapper">
                             <avatar-icon :avatar_icon="user.avatar_icon" />
                         </div>
-                        <div class="col-6">
-                            <h4>{{ user.user_name }}</h4>
+                        <div class="col-7">
+                            <router-link :to="{ name: 'user_detail', params: { userId: user.id }}">
+
+                                <h4>{{ user.user_name }}</h4>
+                            </router-link>
                         </div>
-                        <div class="col-4" v-if="me && user && me.id != user.id">
+                        <div class="col-3" v-if="me && user && me.id != user.id">
                             <follow-button v-on:follow="follow" v-on:unfollow="unfollow" :user="user" />
                         </div>
                     </div>
