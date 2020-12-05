007a5a1f3ef880269de7cbc9c685c3dd67ed0cf5
kinoshita0623 <kuutarousii@gmail.com>
2020-12-05 23:16:47 +0900

    identityiconを試しに表示できるようにした

--- resources/js/atoms/AvatarIcon.vue
+++ resources/js/atoms/AvatarIcon.vue
@@ -2,20 +2,32 @@
     <div class="wrapper">
         <img 
             class="img img-fluid col-12" 
-            :src="avatar_icon ? avatar_icon : '/ic_avatar.png'" 
-            v-on:error="loadAvatarIcon" >                 
+            :src="image" 
+             >                 
     </div>
 </template>
 <script>
+import Identicon from 'identicon.js';
+import jsHash from 'jssha';
+
 export default {
     props: {
         avatar_icon: {
             required: false
         }
     },
-    methods: {
-        loadAvatarIcon(e){
-            e.target.src = "/ic_avatar.png";
+    
+    computed: {
+        image(){
+            if(this.avatar_icon){
+                return this.avatar_icon;
+            }else{
+                let hash = new jsHash("SHA-256","TEXT");
+                hash.update("Panta");
+                let data = new Identicon(hash.getHash("HEX"), 200).toString();
+                console.log(data);
+                return "data:image/png;base64," + data;
+            }
         }
     }
 }
