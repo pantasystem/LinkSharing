fbc542b4200de5759ee6089eed924f198126baf6
kinoshita0623 <kuutarousii@gmail.com>
2020-12-07 19:53:31 +0900

    mutationを実装した

--- resources/js/store/notes.js
+++ resources/js/store/notes.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
+import axios from 'axios';
 
 export default {
 
-    namespaced: true,
+    namespaced: false,
 
     state(){
         return {
@@ -11,17 +12,35 @@ export default {
         }
     },
 
-    actions: {
+    mutations: {
+        setNotes(state, notes){
+            console.assert(Array.isArray(notes), "notesは配列である必要があります");
+            for(let i = 0; i < notes.length; i ++ ){
+                let note = notes[i];
+                Vue.set(this.notes, new String(note.id), note);
+            }
+        },
+        setNote(state, note){
+            Vue.set(this.notes, new String(note.id), note);
+        }
+    },
 
+    actions: {
+        favorite(state, getters, rootState){
+            // TODO 実装する
+        },
+        unfavorite(state, getters, rootState){
+            // TODO 実装する
+        }
     },
 
     getters: {
         getNotesByIds:(state) => (ids)=>{
             console.assert(Array.isArray(ids), "idsは配列である必要があります。");
-            return ids.map((id)=> state.notes[id]);
+            return ids.map((id)=> state.notes[new String(id)]);
         },
         getNoteById:(state) => (id)=>{
-            return state.notes[id];
+            return state.notes[new String(id)];
         }
     }
 }
