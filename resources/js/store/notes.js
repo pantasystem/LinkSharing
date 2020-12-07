7c394a8d3d5cdccbeb5bf4a6d99f532188af6bdc
kinoshita0623 <kuutarousii@gmail.com>
2020-12-07 20:17:21 +0900

    favorite/unfavoriteを実装

--- resources/js/store/notes.js
+++ resources/js/store/notes.js
@@ -17,20 +17,59 @@ export default {
             console.assert(Array.isArray(notes), "notesは配列である必要があります");
             for(let i = 0; i < notes.length; i ++ ){
                 let note = notes[i];
-                Vue.set(this.notes, new String(note.id), note);
+                Vue.set(state.notes, new String(note.id), note);
             }
         },
         setNote(state, note){
-            Vue.set(this.notes, new String(note.id), note);
+            Vue.set(state.notes, new String(note.id), note);
         }
     },
 
     actions: {
-        favorite(state, getters, rootState){
-            // TODO 実装する
+        favorite({commit , state, getters, rootState}, noteId){
+            axios.post(
+                `/api/users/${userId}`,
+                {},
+                {
+                    headers: { Authorization: `Bearer ${rootState.token }` }
+                    
+                },
+            ).then((res)=>{
+                let note = state.notes[String(noteId)];
+                console.assert(note, '無効なノート' + noteId);
+                note = {
+                    ...note
+                };
+                note.favorite = true;
+                commit('setNote', note);
+            }).catch((e)=>{
+                console.log(e);
+            });
         },
-        unfavorite(state, getters, rootState){
-            // TODO 実装する
+        unfavorite({commit, state, rootState}, noteId){
+            axios.delete(
+                `/api/notes/${noteId}/favorites`,
+                {
+                    headers: { Authorization: `Bearer ${rootState.token }` }
+                }
+            ).then((res)=>{
+                let note = state.notes[String(noteId)];
+                console.assert(note, "無効なノートが指定されています:" + noteId);
+                if(note.id == noteId && res.data.note_id == note.id){
+
+                    note = {
+                        ...note
+                    };
+                    /*note = {
+                        ...note
+                    };*/
+                    note.favorite = false;
+                    commit('setNote', note);
+                    //Vue.set(state.notes, String(note.id), )
+                }
+            }).catch((e)=>{
+                console.log(e);
+            });
         }
     },
 
