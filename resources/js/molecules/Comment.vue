6faa0879111bf9467041f5ab13e1b0d2fa52e0e2
kinoshita0623 <kuutarousii@gmail.com>
2020-12-15 09:36:32 +0900

    コメントの一部を実装した

--- resources/js/molecules/Comment.vue
+++ resources/js/molecules/Comment.vue
@@ -1,8 +1,31 @@
 <template>
-    
+    <article>
+        <div class="row">
+            <div class="col-2">
+                <avatar-icon :user="comment.author" />
+            </div>
+            <div class="col-10">
+                
+            </div>
+        </div>
+        <div>
+            <p v-html="$sanitize(comment.text)"></p>
+        </div>
+    </article>
 </template>
 <script>
+import AvatarIcon from '../atoms/AvatarIcon';
+import AvatarIcon from '../atoms/AvatarIcon.vue';
 export default {
-    
+    props: {
+        comment: {
+            type: Object,
+            required: true
+        }
+    },
+    components: {
+        'avatar-icon': AvatarIcon
+    }
 }
 </script>
+        AvatarIcon
