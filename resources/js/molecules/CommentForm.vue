0dfa69df0706824b570c3b8de2c8627b8220502b
kinoshita0623 <kuutarousii@gmail.com>
2020-12-15 10:08:59 +0900

    コメント用のフォームをざっくりと実装した

--- resources/js/molecules/CommentForm.vue
+++ resources/js/molecules/CommentForm.vue
@@ -1,8 +1,25 @@
 <template>
-    
+    <div>
+        <form @submit.prevent="submit">
+            <b-textarea v-model="text"></b-textarea>
+            <b-button type="submit" :variant="primary">送信</b-button>
+        </form>
+    </div>
 </template>
 <script>
+/**
+ * バリデーションなどはここで行い送信が決定されたときに親コンポーネントにデータを投げる
+ */
 export default {
-    
+    data(){
+        return {
+            text: ''
+        }
+    },
+    methods: {
+        submit(){
+            this.$emit('submit', this.text);
+        }
+    }
 }
 </script>
