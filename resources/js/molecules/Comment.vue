fa457b28a0906ff06acfe62cb81fd5b3f5007e08
kinoshita0623 <kuutarousii@gmail.com>
2020-12-19 17:17:19 +0900

    change レイアウトを調整

--- resources/js/molecules/Comment.vue
+++ resources/js/molecules/Comment.vue
@@ -1,21 +1,24 @@
 <template>
-    <article>
-        <router-link :to="{ name: 'user_notes', params: { userId: comment.author.id}}">
-            <div class="col-2">
+    <article class="row">
+        
+        <div class="col-1">
+            <router-link :to="{ name: 'user_notes', params: { userId: comment.author.id}}">
+            
                 <avatar-icon
                     :user="comment.author"
                 />
-            </div>
             
-            <div class="col-10">
-                {{ comment.author.user_name }}   
-            </div>
+            </router-link>
             
-        </router-link>
-        
-        <div>
-            <p v-html="$sanitize(comment.text.replace(/\n/g, '<br>'))"></p>
         </div>
+        <div class="col-11">
+            <div>{{ comment.author.user_name }}</div> 
+            <div>
+                <p v-html="$sanitize(comment.text.replace(/\n/g, '<br>'))"></p>
+            </div>
+        </div>   
+        
+        
     </article>
 </template>
 <script>
