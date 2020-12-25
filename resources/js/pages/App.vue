6b4fde4006b35e79e22dc3482d0e81739017a0b9
kinoshita0623 <kuutarousii@gmail.com>
2020-12-25 17:57:14 +0900

    change 初期読み込みの後にVueインスタンスを初期化するようにした

--- resources/js/pages/App.vue
+++ resources/js/pages/App.vue
@@ -13,15 +13,13 @@ export default {
 
     created(){
         console.log("created");
-        console.log(`timeline${JSON.stringify(this.$store.state)}`);
-        this.$store.dispatch("loadMe")
-            .catch((e)=>{
-                console.log(`アカウントの読み込みに失敗: ${e}`);
-                if(e.status == 401){
-                    this.$router.push("/login");
-                }
-            });
-        console.log(this.$store);
+        
+        if(this.$store.state.user == null){
+            this.$router.push("/login");
+            return;
+
+        }
+
         this.$store.dispatch('timeline/initTimeline');
         this.$store.dispatch('notification/init');
         this.$store.dispatch('notification/loadNext');
