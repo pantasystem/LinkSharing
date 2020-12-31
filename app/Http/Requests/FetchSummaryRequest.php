d329a7e2b282193c549fe6870e7eb46b2c1a1685
kinoshita <kinoshita062323@gmail.com>
2021-01-01 04:09:22 +0900

    change FetchSummaryRequestを実装した

--- app/Http/Requests/FetchSummaryRequest.php
+++ app/Http/Requests/FetchSummaryRequest.php
@@ -13,7 +13,7 @@ class FetchSummaryRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -25,6 +25,7 @@ class FetchSummaryRequest extends FormRequest
     {
         return [
             //
+            'url' => ['required', 'url']
         ];
     }
 }
