07a90aa042e5fbd4e88f9fdfdd7a6a144576938b
kinoshita <kinoshita062323@gmail.com>
2021-01-02 21:33:35 +0900

    delete 不要なメソッドを削除

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -45,38 +45,7 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'user_name' => ['required', 'alpha_dash','alpha_num', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-            //'device_name' => ['required', 'max:255']
-        ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\Models\User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'user_name' => $data['user_name'],
-            'email' => $data['email'],
-
-            'password' => Hash::make($data['password'])
-            
-        ]);
-    }
+  
 
     public function register(CreateUserRequest $request)
     {
