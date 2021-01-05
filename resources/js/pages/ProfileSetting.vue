0732ec9a753d52ef624e156a1e6d0048b03c8d5c
kinoshita0623 <kuutarousii@gmail.com>
2021-01-05 18:45:39 +0900

    change SettingFormLayoutを適応した

--- resources/js/pages/ProfileSetting.vue
+++ resources/js/pages/ProfileSetting.vue
@@ -1,53 +1,78 @@
 <template>
     <div class="container">
-        <div class="row justify-flex-center pb-4 border-bottom">
-            <div class="col-md-5">
+        <setting-form-layout>
+            <template v-slot:header>
                 <h2>
                     アバターアイコン
                 </h2>
                 <p>
                     ユーザーアイコンとしてプロフィールや投稿などに表示されます。
                 </p>
-            </div>
-            <div class="col-md-7">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="col-4 mx-auto mb-2">
-                            <avatar-icon 
-                                :user="user"
-                            />
+            </template>
+            <template v-slot:body>
+                <div class="col-4 mx-auto mb-2">
+                    <avatar-icon 
+                        :user="user"
+                    />
                         
-                        </div>
-                        
-                        <b-form-file>
-
-                        </b-form-file>
-                        <p>
-                            1Mバイト以下の画像をアップロードすることができます。
-                        </p>
-                    </div>
-                    <div class="card-footer">
-                        <b-button variant="primary">保存</b-button>
-                    </div>
-                    
                 </div>
-            </div>
+                        
+                <b-form-file>
 
-        </div>
+                </b-form-file>
+                <p>
+                    1Mバイト以下の画像をアップロードすることができます。
+                </p>
+            </template>
+            <template v-slot:footer >
+                <div class="text-right">
+                    <b-button class="ml-2">削除</b-button>
+                    <b-button variant="primary">アップロード</b-button>
+
+                </div>
+
+            </template>
+        </setting-form-layout>
+        <setting-form-layout class="mt-4">
+            <template v-slot:header>
+                <h2>
+                    ユーザー名
+                </h2>
+                <p>
+                    投稿やプロフィールに表示されます。<br>
+                    一意である必要があります。
+                </p>
+            </template>
+            <template v-slot:body>
+                <b-form-input
+                    placeholder="ユーザー名"
+                    v-model="user_name"
+                />
+            </template>
+            
+        </setting-form-layout>
+        
     </div>
 </template>
 <script>
 import AvatarIcon from '../atoms/AvatarIcon';
-import SettingForm from '../templates/SettingForm';
+import SettingFormLayout from '../templates/SettingFormLayout';
 import { mapState } from 'vuex';
 export default {
     components: {
         AvatarIcon,
-        SettingForm
+        SettingFormLayout
     },
 
     computed: {
         ...mapState(['user'])
+    },
+    data(){
+        return {
+            avatar_icon_file: null,
+            user_name: this.$store.state.user.user_name,
+
+        };
     }
 }
 </script>
