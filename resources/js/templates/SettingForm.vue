f58d4c3605fccc2080725769557558cced456083
kinoshita0623 <kuutarousii@gmail.com>
2021-01-05 17:57:52 +0900

    設定共通のテンプレートを追加

---
+++ resources/js/templates/SettingForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="row justify-flex-center pb-4 border-bottom">
+            <div class="col-md-5">
+                <slot name="header" />
+            </div>
+            <div class="col-md-7">
+                <div class="card">
+                    <div class="card-body">
+                        <slot name="body" />
+                    </div>
+                    <div class="card-footer">
+                        <slot name="footer" />
+                    </div>
+                    
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
