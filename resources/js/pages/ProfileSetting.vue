16f56e64e607812785dec683ddc39a5c8b5a5183
kinoshita0623 <kuutarousii@gmail.com>
2021-01-07 08:44:09 +0900

    ユーザー設定画面を作成した

--- resources/js/pages/ProfileSetting.vue
+++ resources/js/pages/ProfileSetting.vue
@@ -49,9 +49,62 @@
                     v-model="user_name"
                 />
             </template>
+            <template v-slot:footer>
+                <div class="text-right">
+                    <b-button variant="primary">
+                        変更
+                    </b-button>
+                </div>
+                
+            </template>
             
         </setting-form-layout>
-        
+        <setting-form-layout class="mt-4">
+            <template v-slot:header>
+                <h2>
+                    メールアドレス　
+                </h2>
+                <p>
+                    ログイン時に使用されます。<br>
+                    他のユーザーに公開されることはありません。
+                </p>
+            </template>
+            <template v-slot:body>
+                <b-form-input placeholder="メールアドレス" type="email" />
+            </template>
+        </setting-form-layout>
+        <setting-form-layout class="mt-4">
+            <template v-slot:header>
+                <h2>
+                    パスワード変更
+                </h2>
+                <p>
+                    パスワードを変更します。<br>
+                    パスワードは不可逆暗号化されデータベースに保存されます。
+                </p>
+            </template>
+            <template v-slot:body>
+                <div class="mb-4">
+                    <b-form-input placeholder="現在のパスワード" type="password" />
+                    現在のパスワード
+                </div>
+                <div class="mb-2">
+                    <b-form-input placeholder="変更パスワード" type="password" />
+                    変更パスワード
+                </div>
+                <div>
+                    <b-form-input placeholder="変更パスワードの確認" type="password" />
+                    変更パスワードの確認
+                </div>
+            </template>
+            <template v-slot:footer>
+                <div class="text-right">
+                    <b-button variant="primary">
+                        変更
+                    </b-button>
+                </div>
+            </template>
+        </setting-form-layout>
     </div>
 </template>
 <script>
