6b754eb357424510f8ad5bd91243cb4568b694a7
kinoshita <kinoshita062323@gmail.com>
2021-02-01 06:09:28 +0900

    changed 通信部分を実装した

--- resources/js/organisms/FindUser.vue
+++ resources/js/organisms/FindUser.vue
@@ -4,17 +4,72 @@
             <h3>ユーザーを探す</h3>
             <p>ユーザーが利用しているタグをもとにユーザーを探してみましょう。</p>
         </div>
-        <div>
-            <search-tag />
+        <div class="mb-3">
+            <search-tag @changed="fetch" />
         </div>
+        <div class="card">
+            <div class="card-header">
+                タグに関連するユーザー
+            </div>
+            <div class="card-body">
+                <div v-if="isLoading" class="text-center">
+                    <div class="spinner-border text-primary" role="status">
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                </div>
+                <div v-else>
+                </div>
+            </div>
+
+        </div>
+
     </div>
 </template>
 <script>
 import SearchTag from './SearchTag';
+import UserFollowee from '../components/FolloweeComponent';
+import axios from 'axios';
 
 export default {
     components: {
-        SearchTag
+        SearchTag,
+        UserFollowee
+    },
+    data() {
+        return {
+            userIds: [],
+            isLoading: false,
+            loadVersion: 0
+        }
+    },
+
+    methods: {
+        fetch(tags) {
+            this.loadVersion ++;
+            let lv = this.loadVersion;
+            this.isLoading = true;
+            axios.get('/api/users-relatid-to-tags', {
+                params: { tags: tags.map((tag)=> tag.name) }
+            }).catch((e)=>{
+                console.log(e);
+            }).then((res)=>{
+                if(lv == this.loadVersion){
+                    let users = res.data;
+                    this.$store.commit('users', users);
+                    this.userIds = users.map((u)=> u.id);
+                }
+            }).finally(()=>{
+                if(lv == this.loadVersion){
+                    this.isLoading = false;
+                }
+            })
+        },
+    },
+
+    
+
+    created() {
+        this.fetch(null);
     }
 }
 </script>
