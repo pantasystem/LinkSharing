b5dfa68148c8f9dbbfd77536976098d19462f4b7
kinoshita <kinoshita062323@gmail.com>
2021-02-01 04:03:26 +0900

    change タグの検索と洗濯をできるようにした

--- resources/js/organisms/SearchTag.vue
+++ resources/js/organisms/SearchTag.vue
@@ -8,9 +8,21 @@
                     候補タグ
                 </div>
                 <div class="card-body">
-                    <b-form-input @input="search" hint="タグを検索する"/>
-                    <div>
-                        {{ this.tags}} 
+                    <b-form-input class="mb-2" @input="search" hint="タグを検索する"/>
+                    <div v-if="isLoading" class="text-center">
+                        <div class="spinner-border text-primary" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                    </div>
+                    
+                    <div v-else>
+                        <b-form-checkbox v-for="tag in tags" :key="tag.name" 
+                            :checked="selected.some((t)=> t.name == tag.name)"
+                            @input="changed(tag)"
+                        >
+                            {{ tag.name}}
+                        </b-form-checkbox>
+
                     </div>
                 </div>
             </div>
@@ -23,7 +35,14 @@
                     選択済みタグ
                 </div>
                 <div class="card-body">
+                    <input v-for="tag in selected" :key="tag.name" 
+                        type="checkbox"
+                        :checked="selected.some((t)=> t.name == tag.name)"
 
+                        @input="changed(tag)"
+                        value="hoge"
+
+                    />
                 </div>
             </div>
         </div>
@@ -67,9 +86,16 @@ export default {
                 }
             });
         },
+        changed(tag) {
+            if(this.selected.some((t)=> t.name == tag.name)){
+                this.unselect(tag);
+            }else{
+                this.select(tag);
+            }
+        },
         select(tag) {
-            let index = this.selected.indexOf(tag);
-            if(index < 0){
+            console.log(tag);
+            if(!this.selected.some((e)=> tag.name == e.name)){
                 this.selected.push(tag);
                 this.$emit('selected', this.selected);
             }
